0cdb3d7b29c6a552f3d4ee62ba1e837428cb1729
Fahari <karonjekevin67@gmail.com>
2019-02-14 04:56:27 +0300

    initialize Migrate class

--- manage.py
+++ manage.py
@@ -6,8 +6,10 @@ from  flask_migrate import Migrate, MigrateCommand
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+migrate = Migrate(app,db)
 
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
