14872cdae53f2bb4f56a90f7a866e517066f67a2
Fahari <karonjekevin67@gmail.com>
2019-02-14 11:03:38 +0300

    import WERKEZEUG'S security module and import functions that will hash and verify passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -7,3 +8,15 @@ class User(db.Model):
     username = db.Column(db.String(20),index = True)
     email = db.Column(db.String(20),unique = True,index = True)
     password_hash = db.Column(db.String(30))
+
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.password_hash = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.password_hash,password)
