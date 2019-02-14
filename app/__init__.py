8efed3154376e29b028273478486369d02fdf103
Fahari <karonjekevin67@gmail.com>
2019-02-14 12:31:40 +0300

    initialize the Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-# from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 login_manager = LoginManager()
@@ -19,7 +19,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
-    # bootstrap.init_app(app)
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
