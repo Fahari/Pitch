06253a9600f9870a280c4c42489838949abdc08b
Fahari <karonjekevin67@gmail.com>
2019-02-14 12:51:43 +0300

    create base.html view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,12 @@
 from flask_login import login_required
+from flask import render_template
+from app import app
 
-
+@app.route('/')
 # @login_required
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('base.html')
