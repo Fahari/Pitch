b35ef7649b3ddd456afd9f477b4c4d482ff0939a
Fahari <karonjekevin67@gmail.com>
2019-02-15 03:59:42 +0300

    setup some configuration files to connect to the gmail smtp server.

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY='lambalolo'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     pass
 
 
