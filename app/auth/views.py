8efef86294595d9e9114ea019e14bc9c4f932345
Fahari <karonjekevin67@gmail.com>
2019-02-15 11:13:43 +0300

    deploy

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,7 +19,6 @@ def login():
     title = "Impress me login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
